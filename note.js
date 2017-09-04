69d934c58f33d8886af25514278d5cfcd42ed367
CloudWerewolf <joshharc@gmail.com>
2017-09-04 13:25:22 +1000

    working js functions

--- note.js
+++ note.js
@@ -1,18 +1,31 @@
 var notelist = [];
-function saveNote(title, text) {
-    notelist.push({"title": title, "note": text});
+//commented out lines to be used later
+//function loadNote(noteid) {
+//    alert(noteid);
+//}
+function remove(id) {
+    var list = document.getElementsByClassName(id);
+    for(var i = list.length - 1; 0 <= i; i--) {
+        if(list[i] && list[i].parentElement) {
+            list[i].parentElement.removeChild(list[i]);    
+        }
+    }
+}
+function saveNote(head, text) {
+    notelist.unshift({"title": head, "note": text});
 }
 function displayNotes() {
     var div = document.getElementById("notepage");
-//    var del = document.getElementsByName("notes");
-//    div.removeChild(del);
+    remove('notelist'); 
     for (var notes in notelist){
         var text = document.createElement("textarea");
         text.name = "notes"
         text.cols = 45;
         text.rows = 5;
-        text.id = "notelist";
-        text.value = notelist[notes].title + '\n' + notelist[notes].note;
+        text.className = "notelist";
+        this.id = notes;    
+        text.value = notelist[notes].title.toUpperCase() + '\n' + notelist[notes].note;
+//        text.setAttribute('onclick', 'loadNote(id);')
         div.appendChild(text);
     }
 }
