3d946f7145c2a8c0e8b3624536fe3ac527f4e9fc
CloudWerewolf <joshharc@gmail.com>
2017-09-04 13:25:22 +1000

    fixes

--- note.js
+++ note.js
@@ -1,31 +1,47 @@
 var notelist = [];
-//commented out lines to be used later
-//function loadNote(noteid) {
-//    alert(noteid);
-//}
-function remove(id) {
-    var list = document.getElementsByClassName(id);
-    for(var i = list.length - 1; 0 <= i; i--) {
-        if(list[i] && list[i].parentElement) {
-            list[i].parentElement.removeChild(list[i]);    
-        }
+
+function noteListen() {
+    document.getElementById('note').addEventListener("keydown", function (event) {
+    if (event.keyCode == 13 && event.ctrlKey) {
+        document.getElementById('save').click();
+        event.preventDefault();
     }
+    });
+    document.getElementById('notehead').addEventListener("keydown", function (event) {
+    if (event.keyCode == 13 && event.ctrlKey) {
+        document.getElementById('save').click();
+        event.preventDefault();
+    }
+    });
+}
+
+function addNote(head, text) {
+    saveNote(head, text);
+    displayNotes();
 }
 function saveNote(head, text) {
     notelist.unshift({"title": head, "note": text});
+    var head = document.getElementById("notehead");
+    var body = document.getElementById("note");
+    head.value = "";
+    body.value = "";
 }
 function displayNotes() {
     var div = document.getElementById("notepage");
-    remove('notelist'); 
+    while (div.hasChildNodes()) {
+        div.removeChild(div.lastChild);
+    }
     for (var notes in notelist){
         var text = document.createElement("textarea");
         text.name = "notes"
         text.cols = 45;
         text.rows = 5;
         text.className = "notelist";
-        this.id = notes;    
-        text.value = notelist[notes].title.toUpperCase() + '\n' + notelist[notes].note;
-//        text.setAttribute('onclick', 'loadNote(id);')
+        this.id = notes;
+        if (notelist[notes].title != "") {
+            text.value = notelist[notes].title.toUpperCase() + '\n' + notelist[notes].note;
+        }
+        else { text.value = notelist[notes].note;}
         div.appendChild(text);
     }
 }
