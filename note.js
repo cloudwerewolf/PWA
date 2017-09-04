ecc5baa37c6d1a48c87acafaf58a330c9a0e1fac
CloudWerewolf <joshharc@gmail.com>
2017-09-04 13:25:22 +1000

    saveNote

--- note.js
+++ note.js
@@ -1,10 +1,16 @@
-var notelist = [{"title": "hi", "note": "test"}, {"title": "test", "note": "test"}, {"title": "test", "note": "hi"}];
+var notelist = [];
+function saveNote(title, text) {
+    notelist.push({"title": title, "note": text});
+}
 function displayNotes() {
+    var div = document.getElementById("notepage");
+//    var del = document.getElementsByName("notes");
+//    div.removeChild(del);
     for (var notes in notelist){
-        var div = document.getElementById("notepage");
         var text = document.createElement("textarea");
-        text.cols = "45";
-        text.rows = "5";
+        text.name = "notes"
+        text.cols = 45;
+        text.rows = 5;
         text.id = "notelist";
         text.value = notelist[notes].title + '\n' + notelist[notes].note;
         div.appendChild(text);
